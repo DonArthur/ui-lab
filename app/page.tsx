a7204c237a1ef14ec451c2f05268532a215e4a80
DonArthur <geraldy.miero@gmail.com>
2025-12-31 00:36:12 +0700

    update: styling and add aos library (animate on scroll)

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,15 @@ export default function Home() {
   return (
     <div className="w-[90%] mx-auto">
       <Navbar />
-      <header className="mt-24 mr-0 mb-48 ml-28">
-        <h1 className="text-white text-5xl">UI & UX LAB</h1>
-        <p className="text-white text-base font-light mt-2.5 w-[60%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat obcaecati dolorum impedit fuga aut repellendus voluptates eligendi libero expedita, velit modi! Vel molestiae atque facilis cupiditate libero, culpa consequuntur sed.</p>
+      <header data-aos="zoom-in" className="my-14 ml-12 md:mt-24 mr-0 md:mb-48 md:ml-28 md:text-left">
+        <h1 className="text-white text-3xl md:text-5xl">UI & UX LAB</h1>
+        <p className="text-white text-base font-light mt-2.5 w-[90%] md:w-[60%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat obcaecati dolorum impedit fuga aut repellendus voluptates eligendi libero expedita, velit modi! Vel molestiae atque facilis cupiditate libero, culpa consequuntur sed.</p>
       </header>
-      <div className="w-full grid grid-cols-2 gap-8 mb-24">
-        <Image src="/images/nextjs.jpg" alt="project" width={800} height={500} />
-        <Image src="/images/nextjs.jpg" alt="project" width={800} height={500} />
-        <Image src="/images/nextjs.jpg" alt="project" width={800} height={500} />
-        <Image src="/images/nextjs.jpg" alt="project" width={800} height={500} />
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-8 mb-24">
+        <Image data-aos="zoom-in" src="/images/nextjs.jpg" alt="project" width={800} height={500} />
+        <Image data-aos="zoom-in" src="/images/nextjs.jpg" alt="project" width={800} height={500} />
+        <Image data-aos="zoom-in" src="/images/nextjs.jpg" alt="project" width={800} height={500} />
+        <Image data-aos="zoom-in" src="/images/nextjs.jpg" alt="project" width={800} height={500} />
       </div>
       <Footer />
     </div>
